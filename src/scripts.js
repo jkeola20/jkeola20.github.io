322da5199f0fc7ac24941ce5c59e1b28eb297e7e
zzhang201 <zzhang201@students.kgi.edu>
2022-05-03 02:46:24 -0700

    commit resolve for conflict

--- src/scripts.js
+++ src/scripts.js
@@ -15,20 +15,3 @@ const next = function() {
     $("#" + `q${current + 1}`).style.display = 'block';
 }
 */
-
-$(document).ready(function() {
-    $(":submit").click(function() {
-        let current = 0;
-        for (let i = 1; i <= 5; i++) {
-            if ($("#" + `q${i}`).is(":visible")) {
-                current = i;
-            }
-        }
-        if (!current) {
-            $("#welcome_message").show();
-        } else {
-            $("#" + `q${current}`).hide();
-        }
-        $("#" + `q${current + 1}`).show();
-    });
-});
