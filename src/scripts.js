00baba4cc758486f0c7c6116688479797828b37b
Janette Keola <jkeola20@students.kgi.edu>
2022-05-02 20:51:39 -0700

    commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,22 @@
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"></script>
 
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width + "%";
-      }
-    }
-  }
+// COOKIES
+// When a user submits the profile creation form, set a cookie
+$('#submit-input').click(function () {
+  // Set profile info from inputs
+  var userName = $('#name-input').val();
+  var phone = $('#phone-input').val();
+  // Create profile cookie
+  Cookies.set('userName', userName);
+  Cookies.set('phone', phone);
+  // Reload page so that cookie can take effect
+  location.reload();
+});
+// If there is a cookie, show welcome message
+var userName = Cookies.get('userName');
+if (userName) {
+  // Hide new user input form
+  $('#new-user-input').hide();
+  // Show welcome message
+  $('#welcome-text').text('Welcome, ' + userName);
 }
