b23133b036456303a4685e368ed062e81db2ca44
zzhang201 <zzhang201@students.kgi.edu>
2022-05-03 23:22:41 -0700

    commit on making js on abstraction page to work

---
+++ src/abstraction-scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(":submit").click(function() {
+        let current = 0;
+        for (let i = 1; i <= 3; i++) {
+            if ($("#" + `q${i}`).is(":visible")) {
+                current = i;
+            }
+        }
+        if (!current) {
+            $("#welcome_message").hide();
+            $("#welcome_button").hide();
+        } else {
+            $("#" + `q${current}`).hide();
+        }
+        $("#" + `q${current + 1}`).show();
+    });
+    $("form").submit(e => e.preventDefault());
+    $("#q3 > :submit").click(function() {
+        window.location.href = '/ending.html';
+        return false;
+    })
+});
